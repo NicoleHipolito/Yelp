811ad48517e623a6d61cf7048c5edaec1996f7b7
NicoleHipolito <nhipolito1@gmail.com>
2018-02-18 20:35:48 -0800

    Finished yelp app

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,44 +8,91 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController {
+class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
     
     var businesses: [Business]!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var searchBar = UISearchBar()
+    var searchController: UISearchController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
-            
-            self.businesses = businesses
-            if let businesses = businesses {
-                for business in businesses {
-                    print(business.name!)
-                    print(business.address!)
-                }
-            }
-            
-            }
-        )
+        tableView.delegate = self
+        tableView.dataSource = self
         
-        /* Example of Yelp search with more search options specified
-         Business.searchWithTerm("Restaurants", sort: .distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: Error!) -> Void in
-         self.businesses = businesses
-         
-         for business in businesses {
-         print(business.name!)
-         print(business.address!)
-         }
-         }
-         */
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 120
+        
+        getRestaurantQuery(userInput: "Thai")
+        
+        // create the search bar programatically since you won't be
+        // able to drag one onto the navigation bar
+        
+        searchBar.sizeToFit()
+        searchBar.placeholder = "Restaurant"
+        searchBar.showsCancelButton = true
+        searchBar.delegate = self
+        // the UIViewController comes with a navigationItem property
+        // this will automatically be initialized for you if when the
+        // view controller is added to a navigation controller's stack
+        // you just need to set the titleView to be the search bar
+        navigationItem.titleView = searchBar
+        
         
     }
     
+    func getRestaurantQuery(userInput: String){
+        Business.searchWithTerm(term: userInput, completion: { (businesses: [Business]?, error: Error?) -> Void in
+            
+            self.businesses = businesses
+            //put data into table:
+            self.tableView.reloadData()
+
+//            for business in businesses! {
+//                print(business.name!)
+//                print(business.address!)
+//            }
+            
+        }
+            
+        )
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        searchBar.placeholder = "Restaurant"
+        searchBar.resignFirstResponder()
+        self.tableView.reloadData()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        getRestaurantQuery(userInput: searchBar.text!)
+        print(searchBar.text!)
+        self.tableView.reloadData()
+        searchBar.resignFirstResponder()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if businesses != nil{
+            return businesses!.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessCell", for: indexPath) as! BusinessCell
+        cell.business = businesses[indexPath.row]
+        
+        return cell
+    }
     /*
      // MARK: - Navigation
      
